05574d1c7376cac03ed4878d88f49d23b6cf533a
Christian <christiannguyendinh@yahoo.com>
2017-03-09 02:52:32 -0500

    skeleton html, css, and routing changes

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 var express = require('express');
 var server = express();
+var path = require('path')
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('test.db');
 
-var router = express.Router();
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.use('/api', router);
-
-server.listen(3000, function() {
-    console.log("server started on port 3000");
+server.get('/', function(req, res) {
+    res.sendFile('index.html')
 });
 
-router.get('/', function(req, res) {
-    res.json({ message: "Hello" });
-});
-
-router.route('/users/:name')
+server.route('/api/:name')
     .post(function(req, res) {
         console.log(req.params.name);
         res.json({ message: "good" });
@@ -24,7 +19,10 @@ router.route('/users/:name')
         res.json({student: req.params.name});
     });
 
-server.use('/api', router);
+server.listen(3000, function() {
+    console.log("server started on port 3000");
+});
+
 
 function accessDatabase() {
 	db.serialize(() => {
