1ee9bbde89e365de9b4fb5a61b097b83d73829b7
Christian <christiannguyendinh@yahoo.com>
2017-04-23 22:57:32 -0400

    initial postgres WIP

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var server = express();
 var path = require('path');
 var request = require('request');
 var bodyparser = require('body-parser');
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('test.db');
+var routes = require('./routes/routes.js');
 
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({
@@ -46,139 +45,29 @@ server.get('/graphs/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/graphs.html'));
 });
 
-server.route('/api/:name/')
-    .post(function(req, res) {
-        console.log(req.body);
-        console.log(req.body.name);
-        console.log(req.body.date);
-        addGame(req.body);
-        res.json({ message: "good" });
-    })
-    .get(function(req, res) {
-        res.json({student: req.params.name});
-    });
+////////// API Version 1 : SQLITE3 ////////////////////
+
+server.post('/api/v1/:name/', routes.api_v1.postData);
 
 // Return the winrate in as a percent. Ex. {'count': 62.00}
-server.get('/api/winrate/:name', function(req, res) {
-    let ratioArr = [];
-    let ratio = "invalid";
-    db.serialize(() => {
-        db.each('SELECT result FROM combined WHERE name = \"' + req.params.name + '\"', (err, row) => {
-            if (!err) {
-                ratioArr.push(row.result);
-            } else {
-                console.log(err)
-            }
-        }, (err, rowc) => {
-            if (!err && rowc > 0) {
-                ratio = ratioArr.reduce((acc, x) => { return x == 'win' ? acc + 1 : acc }, 0);
-                ratio = (ratio / ratioArr.length).toFixed(2)*100;
-            } 
-            res.json({count: ratio});
-        });
-    });
-});
+server.get('/api/v1/winrate/:name', routes.api_v1.winRate);
 
 // Return the number of wins and losses. Ex. {'win': 5, 'lose': 12}
-server.get('/api/winloss/:name', function(req, res) {
-    let winloss = {win: 0, lose: 0};
-    db.serialize(() => {
-        db.each('SELECT result FROM combined WHERE name = \"' + req.params.name + '\"', (err, row) => {
-            if (!err) {
-                row.result == 'win' ? winloss['win']++ : winloss['lose']++;
-            } else {
-                console.log(err)
-            }
-        }, (err, rowc) => {
-            res.json(winloss);
-        });
-    });
-});
+server.get('/api/v1/winloss/:name', routes.api_v1.winLoss);
 
 // Returns json data about match records against each class. 
 // Ex. {... 'warlock': {'gamesWonAs': 4, 'gamesPlayedAs': 5, 'gamesLostAgainst': 2, 'gamesPlayedAgainst': 7}, ...}
-server.get('/api/classresults/:name', function(req, res) {
-    let record = {
-        'warlock': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'warrior': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'paladin': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'druid': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'rogue': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'priest': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'hunter': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'mage': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 },
-        'shaman': { 'gamesWonAs': 0, 'gamesPlayedAs': 0, 'gamesLostAgainst': 0, 'gamesPlayedAgainst': 0 }
-    };
-    db.serialize(() => {
-        db.each('SELECT * FROM combined WHERE name = \"' + req.params.name + '\"', (err, row) => {
-            if (!err) {
-                // id | name | deck | opponent | result | date
-                record[row.deck]['gamesPlayedAs']++;
-                record[row.opponent]['gamesPlayedAgainst']++;
-                row.result == 'win' ? record[row.deck]['gamesWonAs']++ : record[row.opponent]['gamesLostAgainst']++;
-            } else {
-                console.log(err)
-            }
-        }, (err, rowc) => {
-            res.json(record);
-        });
-    });
-});
+server.get('/api/v1/classresults/:name', routes.api_v1.classResults);
 
 // Day/Time functions here for later
-server.get('/api/timestats/:name', function(req, res) {
-    let dateObj = {};
-    dateObj['dayCount'] = (new Array(7)).fill(0);
-    dateObj['monthCount'] = (new Array(28)).fill(0);
-    dateObj['yearCount'] = (new Array(12)).fill(0);
-    let today = new Date();
-    db.serialize(() => {
-        db.each('SELECT date FROM combined WHERE name = \"' + req.params.name + '\"', (err, row) => {
-            if (!err) {
-                // id | name | deck | opponent | result | date
-                // Sat Mar 11 2017 03:15:36
-                let d = new Date(row.date);
-                let dayDiff = Math.floor(Math.abs(today - d) / 86400000);
-                let monthDiff = Math.floor(Math.abs(today - d) / 2419000000);
+server.get('/api/v1/timestats/:name', routes.api_v1.timeStats);
 
-                if (dayDiff < 7)
-                    dateObj['dayCount'][6 - dayDiff]++;
+////////// API Version 2 : POSTGRES ////////////////////
 
-                if (dayDiff < 28)
-                    dateObj['monthCount'][27 - dayDiff]++;
-
-                if (monthDiff < 12)
-                    dateObj['yearCount'][11 - monthDiff]++;
-
-            } else {
-                console.log(err)
-            }
-        }, (err, rowc) => {
-            res.json(dateObj);
-        });
-    });
-});
+// Test for postgres
+server.get('/api/v2/test/:name', routes.api_v2.test);
 
 server.listen(3000, function() {
     console.log("server started on port 3000");
 });
-
-addGame = function(data) {
-    db.serialize(() => {
-        console.log('INSERT INTO combined (name, deck, opponent, result, date) VALUES ("' + 
-            data.name + '", "' + data.mdeck + '", "' + data.tdeck + '", "' + data.result + '", "' + data.date + '")');
-        db.run('INSERT INTO combined (name, deck, opponent, result, date) VALUES ("' + 
-            data.name + '", "' + data.mdeck + '", "' + data.tdeck + '", "' + data.result + '", "' + data.date + '")');
-
-        console.log("game added");
-    });
-};
-
-cleanup = function() {
-    console.log("closing database and cleaning up");
-    db.close;
-    process.exit();
-};
-// this can be done better
-process.on('SIGINT', cleanup);
 
---
+++ routes/api/api_v2.js
@@ -0,0 +1,25 @@
+const pg = require('pg');
+const conString = 'postgres://<user>:<pass>@localhost:5432/<dbname>'
+
+exports.test = (req, res) => {
+    pg.connect(conString, (err, client, done) => {
+        if (err) return console.error(err);
+
+        let data = [];
+        client.query('SELECT * FROM test;', (err, result) => {
+            if (!err) {
+                for (let i = 0; i < result.rowCount; i++) {
+                    //console.log("GOT DATA: id: %s, name: %s", result.rows[i].id, result.rows[i].name);
+                    data.push({'id': result.rows[i].id, 'name': result.rows[i].name})
+                }
+            }
+        })
+        .then(() => {
+            client.query('INSERT INTO test (id, name) VALUES ($1, $2);', [69, req.params.name]);
+        })
+        .then(() => {
+            done();
+            res.json(data);
+        });
+    });
+}
