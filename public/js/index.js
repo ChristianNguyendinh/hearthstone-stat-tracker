98cd740a51d15a7cbac7f342445b1b3304f14223
Christian <christiannguyendinh@yahoo.com>
2017-05-17 21:31:15 -0400

    pie chart integration with front end data strucs

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-var chartData = {'winloss': [0, 0], 
+var chartData = {'winloss': [0, 0], 'winrate': [0, 0], 
     'classAgainst': [
         {name: 'warlock', value: 0, color: 'purple', image: '/images/icons/icon_warlock.png'},
         {name: 'warrior', value: 0, color: 'maroon', image: '/images/icons/icon_warrior.png'},
@@ -57,7 +57,7 @@ $('#mainForm').submit(function(e) {
     }
 
     obj['date'] = (new Date()).toString().split(" G")[0];
-    console.log(obj);
+    //console.log(obj);
 
     // clear button select
     var buttons = document.getElementsByClassName("hidden-input");
@@ -71,7 +71,11 @@ $('#mainForm').submit(function(e) {
         console.log(d);
     });
 
-    updateBarChart();
+    // Update Winrate data
+    var total = chartData['winloss'][0] + chartData['winloss'][1];
+    chartData['winrate'] = [parseFloat((chartData['winloss'][0] / total).toFixed(2)), parseFloat((chartData['winloss'][1] / total).toFixed(2))];
+
+    updateCharts();
 
     e.preventDefault();
 });
@@ -81,15 +85,20 @@ $('#mainForm').submit(function(e) {
 var margin = {top: 75, right: 20, bottom: 40, left: 80};
 var width = 1100 - margin.right - margin.left;
 var height = 600 - margin.top - margin.bottom;
+var radius = Math.min(width, height) / 2;
 
 // init scales
 var scaleX = d3.scaleLinear().range([0, width]);
 var scaleY = d3.scaleBand().rangeRound([0, height]).padding(0.1);
+var color = d3.scaleOrdinal().range(["#4286f4", "#ef3b56"]);
 
 // make axis objects
 var xAxis = d3.axisBottom(scaleX);
 var yAxis = d3.axisLeft(scaleY).tickFormat("");
 
+// Bar Chart -------------------------------------------------
+
+// Tooltip for bar chart
 var tip = d3.tip()
     .attr('class', 'd3-tip')
     .direction('e')
@@ -99,7 +108,7 @@ var tip = d3.tip()
     });
 
 // select group inside of content for the chart
-var chart = d3.select("#chart")
+var barChart = d3.select("#barChart")
     // .attr("width", width + margin.left + margin.left)
     // .attr("height", height + margin.top + margin.bottom)
     .attr("preserveAspectRatio", "xMinYMin meet")
@@ -112,29 +121,29 @@ scaleX.domain([0, d3.max(chartData['classAgainst'], function(d) { return d.value
 scaleY.domain(chartData['classAgainst'].map(function(d) { return d.name }));
 
 // Append the x axis 
-chart.append("g")
+barChart.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
 // Append the Y axis
-chart.append("g")
+barChart.append("g")
     .attr("class", "y axis")
     .attr("transform", "translate(-2,0)")
     .call(yAxis);
 
 // Append tooltip
-chart.call(tip);
+barChart.call(tip);
 
 // Title
-chart.append("g")
+barChart.append("g")
     .attr("transform", "translate(" + (width * .25) + "," + (0 - margin.bottom) + ")")
     .append("text")
         .html("Games Played Against")
         .attr("class", "title")
 
 // Tick Images
-chart.select(".y").selectAll(".tick")
+barChart.select(".y").selectAll(".tick")
     .data(chartData['classAgainst'])
     .append("svg:image")
         .attr("width", 38)
@@ -144,7 +153,7 @@ chart.select(".y").selectAll(".tick")
         .attr("xlink:href", function(d) { return d.image });
 
 // create the bar chart
-chart.selectAll("rect")
+barChart.selectAll("rect")
     .data(chartData['classAgainst'])
     .enter()
     .append("rect")
@@ -155,15 +164,71 @@ chart.selectAll("rect")
         .on("mouseover", tip.show)
         .on("mouseout", tip.hide);
 
-// update
-function updateBarChart() {
+// Pie Chart -------------------------------------------------
+
+var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 100);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d; });
+
+var pieChart = d3.select("#pieChart")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 " + width + " " + height)
+    .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// Tooltip for donut chart
+var tooltip = pieChart.append('text')
+    .attr('class', 'tooltipText')
+    .attr('x', -40)
+    .attr('y', 10);
+
+var tipContainer = pieChart.append('rect')
+    .attr('x', -60)
+    .attr('y', -65)
+    .attr('class', 'tooltipContainer')
+    .attr('rx', '15')
+    .attr('ry', '15');
+
+var tooltip = pieChart.append('text')
+    .attr('class', 'tooltipText')
+    .attr('x', -40)
+    .attr('y', 10);
+
+var g = pieChart.selectAll(".arc")
+    .data(pie(chartData['winrate']))
+    .enter().append("g")
+    .attr("class", "arc");
+
+g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data); })
+    .on("mouseover", function(d) {
+        var total = d.data;
+        tooltip.html(d.data * 100 + '%');
+        tooltip.style('display', 'block');
+        tipContainer.style('display', 'block');
+    })
+    .on("mouseout", function() {
+        tooltip.style('display', 'none');
+        tipContainer.style('display', 'none');
+    });
+
+// Update Charts ---------------------
+
+function updateCharts() {
+
+    // Update Bar Chart ==============
 
     // remap the domains for new data
     scaleX.domain([0, d3.max(chartData['classAgainst'], function(d) { return d.value })]);
     scaleY.domain(chartData['classAgainst'].map(function(d) { return d.name }));
     
     // get the joins for the new data
-    var r = chart.selectAll("rect")
+    var r = barChart.selectAll("rect")
         .data(chartData['classAgainst']);
     
     // enter for any new nodes    
@@ -185,14 +250,30 @@ function updateBarChart() {
         .style("fill", function(d) { return d.color });
 
     // remove old axis
-    chart.selectAll("x axis").remove()
+    barChart.selectAll("x axis").remove()
 
     // update new axis
-    chart.append("g").transition().duration(1000)
+    barChart.append("g").transition().duration(1000)
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
+    // Update Pie Chart ==============
+
+    var p = pieChart.selectAll(".arc")
+        .data(pie(chartData['winrate']));
+
+    // No new or removed data, so no need for enter or exit
+    p.select("path").transition().duration(750).attrTween("d", arcTween)
+        .style("fill", function(d, i) { return color(i); })
+
 }
 
+function arcTween(a) {
+    var i = d3.interpolate(this._current, a);
+    this._current = i(0);
+    return function(t) {
+        return arc(i(t));
+    };
+}
 
